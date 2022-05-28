419d28670bd919c012afced1e11a38a63bd535dd
Rakesh Kumar Raushan <rkraushan@altametrics.com>
2022-05-28 13:36:03 +0530

    Initial Commit

---
+++ WordGame/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//
+//	ViewController.swift
+//	
+//
+//	Created By Altametrics on 5/28/22
+//	
+//
+
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
